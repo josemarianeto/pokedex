d97a7a60978893d68e315fd7aac72142fe123198
archiism <42035282+archiism@users.noreply.github.com>
2021-10-08 13:02:02 -0300

    fixed cards api bug

--- src/cardpkm.js
+++ src/cardpkm.js
@@ -42,3 +42,33 @@
     <progress  class="nes-progress is-primary progressoCard" value={stats.base_stat} max="100"/> </Box>)  })}
    
   </Typography> 
+
+
+{(pkmFull.pokemon_v2_pokemon.map((pkm) => {
+  return (
+    <Grid  id={pkm.id} item lg={3} md={4} sm={6} xs={12} >
+      
+      <Card elevation={24} className=" cardpk" id={pkm.id}  xs={{ padding: 0 }}>
+        <CardHeader id={pkm.id} sx={{ fontSize: '22px' }} xs={{ padding: 0, fontSize: '12px' }}
+          avatar={<Avatar className="chAvatar" sx={{ width: 90, height: 90 }} xs={{display: 'none'}}>
+            <img src={""} alt={`Pokemon Icon ${pkm.name}`}/>
+          </Avatar>}
+          title={<span className="spanT">{pkm.name}</span>}
+          subheader={pkm.pokemon_v2_pokemontypes.map((type) => { return (<span className={`spanT types ${type.pokemon_v2_type.name}`} id={type.pokemon_v2_type.id}>{ type.pokemon_v2_type.name + " "}</span>) })}
+        />
+        <CardMedia className="" 
+          component="img"
+          height="100%"
+          width="100%"
+          image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pkm.id}.png`}
+          
+          alt={"pokemon : " + pkm.name}
+        />
+
+        <CardContent>
+        <Box nowrap className="btnDiv"><Button variant="contained">Check</Button>
+       </Box>
+        
+        </CardContent>
+      </Card>
+    </Grid>)}))}
